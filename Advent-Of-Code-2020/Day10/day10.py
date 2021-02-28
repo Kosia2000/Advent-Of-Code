77a53e1939ff65fd5f5ab6eef44a77ca829ad986
Kosia2000 <kosia.babiuch@gmail.com>
2021-02-28 20:47:13 +0100

    Advent of Code Day10

--- Advent-Of-Code-2020/Day10/day10.py
+++ Advent-Of-Code-2020/Day10/day10.py
@@ -5,8 +5,7 @@ def get_data():
         return data
 
 
-def fix_list():
-    new_data = get_data()
+def fix_list(new_data):
     new_data.append(0)
     new_data.append(max(new_data)+3)
     new_data.sort()
@@ -14,7 +13,7 @@ def fix_list():
 
 
 def count_difference():
-    numbers = fix_list()
+    numbers = fix_list(get_data())
     byOne = 0
     byThree = 0
 
@@ -27,4 +26,26 @@ def count_difference():
 
     return byOne * byThree
 
+
+checked = {}
+
+def get_way(position=0):
+    all = 0
+    data = fix_list(get_data())
+
+    if position == len(data)-1:
+        return 1
+
+    if position in checked:
+        return checked[position]
+
+    for i in range(position+1, len(data)):
+        if data[i] - data[position] <= 3:
+            all += get_way(i)
+
+    checked[position] = all
+    return all
+
+
 print("Part one: {}".format(count_difference()))
+print("Part two: {}".format(get_way()))
